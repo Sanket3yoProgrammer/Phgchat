75df1c6e78a4d2c68902026c76a6d5a75b9dc807
Sanket Kumar Padhan <152056082+Sanket3yoProgrammer@users.noreply.github.com>
2024-06-26 21:14:56 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex h-screen flex-col items-center justify-center">
-      <h1 className="mb-1 text-6xl font-extrabold text-blue-500">Flow Chat</h1>
+      <h1 className="mb-1 text-6xl font-extrabold text-blue-500">Chat PHG</h1>
       <p className="mb-10">The coolest chat app on the planet</p>
       <Button as={Link} href="/chat">
         Start chatting
